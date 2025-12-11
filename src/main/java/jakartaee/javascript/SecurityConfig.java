2ef909f2a1b82cc4091a267a60821e5eec9c90f8
Reza Rahman <reza_rahman@mail.com>
2025-12-11 17:25:51 -0500

    Fix bug

--- src/main/java/jakartaee/javascript/SecurityConfig.java
+++ src/main/java/jakartaee/javascript/SecurityConfig.java
@@ -2,11 +2,13 @@ package jakartaee.javascript;
 
 import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.security.enterprise.authentication.mechanism.http.BasicAuthenticationMechanismDefinition;
 import jakarta.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
 
 @ApplicationScoped
 @DataSourceDefinition(name = "java:app/jdbc/JavaScriptDb", className = "org.h2.jdbcx.JdbcDataSource", url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", user = "sa", password = "")
-@DatabaseIdentityStoreDefinition(dataSourceLookup = "java:app/jdbc/JavaScriptDb", callerQuery = "SELECT PASSWORD FROM JAVASCRIPT_USERS WHERE USERNAME = ?", groupsQuery = "SELECT GROUP_NAME FROM JAVASCRIPT_USERS WHERE USERNAME = ?")
+@DatabaseIdentityStoreDefinition(dataSourceLookup = "java:app/jdbc/JavaScriptDb", callerQuery = "SELECT PASSWORD FROM JAVASCRIPT_USERS WHERE USERNAME = ?", groupsQuery = "SELECT GROUP_NAME FROM JAVASCRIPT_GROUPS WHERE USERNAME = ?")
+@BasicAuthenticationMechanismDefinition(realmName = "JavaScriptRealm")
 public class SecurityConfig {
 	// Security configuration.
 }
