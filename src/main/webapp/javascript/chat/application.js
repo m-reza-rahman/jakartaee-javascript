e15f88a270f84abc460c4f2029393ee05f1f1d14
Reza Rahman <reza_rahman@mail.com>
2025-12-18 09:51:30 -0500

    Move to WildFly

--- src/main/webapp/javascript/chat/application.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var chatApplication = angular.module('chatApplication', ['chatControllers',
-    'chatServices', 'chatDirectives', 'angular-growl']);
-
-chatApplication.config(['growlProvider', function(growlProvider) {
-    growlProvider.globalTimeToLive(2000);
-}]);
